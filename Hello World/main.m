265f2a8772bfd777e2888d5385697d299f58993b
Mike Wardo <mikewardo@Mikes-MacBook-Air.local>
2014-04-29 17:59:03 -0400

    Initial Commit

---
+++ Hello World/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Mike Wardo on 4/29/14.
+//  Copyright (c) 2014 WARDMANM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
